305300704b73b1e003eafd85527c1b7201550b66
pxuanbach <pxuanbach1412@gmail.com>
2022-02-05 18:52:40 +0700

    Pass from Controller to View

--- Content/pages/Product.jsx
+++ Content/pages/Product.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
-const Product = () => {
-  return <div>This is product page</div>;
+const Product = (props) => {
+    let [state, updateState] = React.useState({
+        products: props.initialProducts,
+    })
+
+    /*
+    let productNodes = state.products.map(product => (
+        <Item name={product.name}></Item>
+    ))
+    */
+    console.log(state.products)
+
+    return (
+        <div>
+            <h1>This is product page</h1>
+        </div>
+    );
 };
+
+const Item = (name) => {
+    return (
+        <li>{name}</li>
+    )
+}
 
 export default Product;
