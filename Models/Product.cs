b1666ad41fe70cb390af581ab3a5c0b90a15eba3
pxuanbach <pxuanbach1412@gmail.com>
2022-02-05 09:10:03 +0700

    Setting project

---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clothing_Management.Models
+{
+    public class Product
+    {
+        public string ProductId { get; set; }
+        public string Name { get; set; }
+        public double OriginPrice { get; set; }
+        public double CostPrice { get; set; }
+        public double Discount { get; set; }
+        public double SalePrice { get; set; }
+        public string ImageDisplay { get; set; }
+        public string QrCodeUrl { get; set; }
+        public string Size { get; set; }
+        public string Quantity { get; set; }
+    }
+}
